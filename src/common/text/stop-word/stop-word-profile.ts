4e9ca683cd9f04e55e1984bde19bd829aa7e10c2
Michal Szczepanski <michal@vane.pl>
2023-02-06 12:45:26 +0100

    feat: add text processing methods

---
+++ src/common/text/stop-word/stop-word-profile.ts
@@ -0,0 +1,78 @@
+`/*
+ * This file is part of the pinmenote-extension distribution (https://github.com/pinmenote/pinmenote-extension).
+ * Copyright (c) 2023 Michal Szczepanski.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */`;
+export class StopWordProfile {
+  static readonly profiles = [
+    'af',
+    'ar',
+    'hy',
+    'eu',
+    'bn',
+    'br',
+    'bg',
+    'ca',
+    'zh',
+    'hr',
+    'cs',
+    'da',
+    'nl',
+    'en',
+    'eo',
+    'et',
+    'fi',
+    'fr',
+    'gl',
+    'de',
+    'el',
+    'gu',
+    'ha',
+    'he',
+    'hi',
+    'hu',
+    'id',
+    'ga',
+    'it',
+    'ja',
+    'ko',
+    'ku',
+    'la',
+    'lt',
+    'lv',
+    'ms',
+    'mr',
+    'no',
+    'fa',
+    'pl',
+    'pt',
+    'ro',
+    'ru',
+    'sk',
+    'sl',
+    'so',
+    'st',
+    'es',
+    'sw',
+    'sv',
+    'th',
+    'tl',
+    'tr',
+    'uk',
+    'ur',
+    'vi',
+    'yo',
+    'zu'
+  ];
+}
---
+++ src/common/text/text-sentence.ts
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the pinmenote-extension distribution (https://github.com/pinmenote/pinmenote-extension).
+ * Copyright (c) 2023 Michal Szczepanski.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+export class TextSentence {
+  static split(inputText: string): string[] {
+    const sentences = inputText.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
+    return sentences;
+  }
+}
