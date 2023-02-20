990016e9272c5fa686c01f4b21042c4c0a7f5d98
Michal Szczepanski <michal@vane.pl>
2023-02-20 20:17:57 +0100

    tech: menu item initialise using list

--- src/options-ui/components/board/search/board-add-element.search.tsx
+++ src/options-ui/components/board/search/board-add-element.search.tsx
@@ -3,14 +3,25 @@ import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { ObjTypeDto } from '../../../../common/model/obj.model';
 import { fnConsoleLog } from '../../../../common/fn/console.fn';
+
+interface AddMenuItem {
+  title: string;
+  type: ObjTypeDto;
+}
+
+const addMenuItemList: AddMenuItem[] = [
+  { title: 'Drawing', type: ObjTypeDto.Drawing },
+  { title: 'Note', type: ObjTypeDto.Note },
+  { title: 'Todo', type: ObjTypeDto.Todo }
+];
 
 export const BoardAddElementSearch: FunctionComponent = () => {
   const [anchorEl, setAnchorEl] = React.useState<undefined | HTMLElement>();
   const open = Boolean(anchorEl);
 
   const handleAddClick = (e: React.MouseEvent<HTMLElement>) => {
-    fnConsoleLog('BoardComponent->AddClick');
     setAnchorEl(e.currentTarget);
   };
 
@@ -18,8 +29,9 @@ export const BoardAddElementSearch: FunctionComponent = () => {
     setAnchorEl(undefined);
   };
 
-  const handleMenuClick = () => {
+  const handleMenuClick = (type: ObjTypeDto) => {
     handleClose();
+    fnConsoleLog('BoardAddElementSearch->MenuItem->click', type);
   };
 
   return (
@@ -28,9 +40,11 @@ export const BoardAddElementSearch: FunctionComponent = () => {
         <AddIcon />
       </IconButton>
       <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
-        <MenuItem onClick={handleMenuClick}>Drawing</MenuItem>
-        <MenuItem onClick={handleMenuClick}>Note</MenuItem>
-        <MenuItem onClick={handleMenuClick}>Todo</MenuItem>
+        {addMenuItemList.map((item, index) => (
+          <MenuItem key={index} onClick={() => handleMenuClick(item.type)}>
+            {item.title}
+          </MenuItem>
+        ))}
       </Menu>
     </div>
   );
