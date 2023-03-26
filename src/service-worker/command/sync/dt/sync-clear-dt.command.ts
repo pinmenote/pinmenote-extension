9cd5ecf7663c268e5cced389146a3b29ba4e31f9
Michal Szczepanski <michal@vane.pl>
2023-03-26 22:29:17 +0200

    feat: add clear sync dt time command

---
+++ src/service-worker/command/sync/dt/sync-clear-dt.command.ts
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the pinmenote-extension distribution (https://github.com/pinmenote/pinmenote-extension).
+ * Copyright (c) 2023 Michal Szczepanski.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import { BrowserStorageWrapper } from '../../../../common/service/browser.storage.wrapper';
+import { ICommand } from '../../../../common/model/shared/common.dto';
+import { ObjectStoreKeys } from '../../../../common/keys/object.store.keys';
+
+export class SyncClearDtCommand implements ICommand<Promise<void>> {
+  async execute(): Promise<void> {
+    await BrowserStorageWrapper.set(ObjectStoreKeys.SYNC_TIME, 0);
+  }
+}
