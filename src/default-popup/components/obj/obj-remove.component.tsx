859d30d6f226650e1e1e92085f82a7d8eeb0f3dc
Michal Szczepanski <michal@vane.pl>
2023-10-13 06:51:11 +0200

    feat: preloader after delete - wait for deletion

--- src/default-popup/components/obj/obj-remove.component.tsx
+++ src/default-popup/components/obj/obj-remove.component.tsx
@@ -14,9 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { ObjDto } from '../../../common/model/obj/obj.dto';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import { ObjTitleFactory } from '../../../common/factory/obj-title.factory';
 
@@ -26,26 +27,47 @@ interface Props {
 }
 
 export const ObjRemoveComponent: FunctionComponent<Props> = (props) => {
+  const [isRemoving, setIsRemoving] = useState<boolean>(false);
+
   let title = '';
   if (props.obj) title = ObjTitleFactory.computeTitle(props.obj);
+
+  const handleRemove = () => {
+    setIsRemoving(true);
+    props.removeCallback(props.obj);
+  };
   return (
-    <div
-      style={{
-        backgroundColor: '#fff',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Typography textAlign="center">Are you sure you want to remove &apos;{title}&apos; ?</Typography>
-      <div style={{ marginTop: 20, display: 'flex', justifyContent: 'space-between', width: 200 }}>
-        <Button variant="outlined" onClick={() => props.removeCallback(props.obj)}>
-          YES
-        </Button>
-        <Button variant="outlined" onClick={() => props.removeCallback()}>
-          NO
-        </Button>
+    <div style={{ display: 'flex' }}>
+      <div
+        style={{
+          width: 300,
+          backgroundColor: '#fff',
+          display: isRemoving ? 'flex' : 'none',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <CircularProgress />
+      </div>
+      <div
+        style={{
+          backgroundColor: '#fff',
+          display: isRemoving ? 'none' : 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Typography textAlign="center">Are you sure you want to remove &apos;{title}&apos; ?</Typography>
+        <div style={{ marginTop: 20, display: 'flex', justifyContent: 'space-between', width: 200 }}>
+          <Button variant="outlined" onClick={handleRemove}>
+            YES
+          </Button>
+          <Button variant="outlined" onClick={() => props.removeCallback()}>
+            NO
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/options-ui/components/board/board/board-item-remove.tsx
+++ src/options-ui/components/board/board/board-item-remove.tsx
@@ -14,11 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { ObjTitleFactory } from '../../../../common/factory/obj-title.factory';
 import { ObjDto } from '../../../../common/model/obj/obj.dto';
+import CircularProgress from '@mui/material/CircularProgress';
 
 interface Props {
   obj?: ObjDto;
@@ -26,26 +27,44 @@ interface Props {
 }
 
 export const BoardItemRemove: FunctionComponent<Props> = (props) => {
+  const [isRemoving, setIsRemoving] = useState<boolean>(false);
   let title = '';
   if (props.obj) title = ObjTitleFactory.computeTitle(props.obj);
+  const handleRemove = () => {
+    setIsRemoving(true);
+    props.removeCallback(props.obj);
+  };
   return (
-    <div
-      style={{
-        backgroundColor: '#fff',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Typography textAlign="center">Are you sure you want to remove &apos;{title}&apos; ?</Typography>
-      <div style={{ marginTop: 20, display: 'flex', justifyContent: 'space-between', width: 200 }}>
-        <Button variant="outlined" onClick={() => props.removeCallback(props.obj)}>
-          YES
-        </Button>
-        <Button variant="outlined" onClick={() => props.removeCallback()}>
-          NO
-        </Button>
+    <div style={{ display: 'flex' }}>
+      <div
+        style={{
+          backgroundColor: '#fff',
+          display: isRemoving ? 'flex' : 'none',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <CircularProgress />
+      </div>
+      <div
+        style={{
+          backgroundColor: '#fff',
+          display: isRemoving ? 'none' : 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Typography textAlign="center">Are you sure you want to remove &apos;{title}&apos; ?</Typography>
+        <div style={{ marginTop: 20, display: 'flex', justifyContent: 'space-between', width: 200 }}>
+          <Button variant="outlined" onClick={handleRemove}>
+            YES
+          </Button>
+          <Button variant="outlined" onClick={() => props.removeCallback()}>
+            NO
+          </Button>
+        </div>
       </div>
     </div>
   );
